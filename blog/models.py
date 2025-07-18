f450e6fdc59cf27e4a447f7a1f87c42148cd47fc
Alexander Svancara <svanalex@isu.edu>
2025-07-17 18:46:26 -0600

    early commit to set up Django project structure

---
+++ blog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib import admin
+from .models import Post, Comment
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    view_count = models.PositiveIntegerField(default=0)
+    category = models.CharField(max_length=100, blank=True, null=True)
+    tags = models.CharField(max_length=200, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    scheduled_publish = models.DateTimeField(blank=True, null=True)
+    is_published = models.BooleanField(default=False)
+    attachments = models.FileField(upload_to='attachments/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
