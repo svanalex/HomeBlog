1497e48c36758608eac083c120c5e028aadcb6fe
Alexander Svancara <svanalex@isu.edu>
2025-07-20 17:51:41 -0600

    Added a lot of content towards a general MVP. Not too much specific functionality yet, but the structure is in place.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 # blog/urls.py
 from django.urls import path
 from . import views
+from django.urls import include
+
+app_name = 'blog'
 
 urlpatterns = [
     path('', views.home_page, name='post-list'),
+    path('posts/', views.PostListView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('posts/create/', views.CreatePost.as_view(), name='post-create'),
 ]
