7fa424d463d9cb90c71ae4676b37479971bc0cec
Alexander Svancara <svanalex@isu.edu>
2025-07-27 13:13:25 -0600

    Fixed blog posts to add image attachments and started updating user profile views.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import views
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'blog'
 
@@ -10,4 +12,6 @@ urlpatterns = [
     path('posts/', views.PostListView.as_view(), name='post-list'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('posts/create/', views.CreatePost.as_view(), name='post-create'),
-]
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
